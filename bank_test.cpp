2858434d1f0a4e6c7966d8591eb8f45965ed3a86
Mr.Jay <JayPraj1999@users.noreply.github.com>
2022-12-08 19:59:45 +0530

    bank

---
+++ bank_test.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+class bank
+{
+	public :
+		
+		string acc_holder_name;
+		string acc_type;
+		long int acc_number;
+		int balance,val;
+		char ch;
+		
+		void assign()
+		{
+			cout<<"Pleae Enter Account holder name : ";
+			cin>>acc_holder_name;
+			cout<<"Please Enter Account Number : ";
+			cin>>acc_number;	
+			cout<<"Please Enter Account type : ";
+			cin>>acc_type;
+			cout<<"\n\nYour Current balance is 00 "<<endl<<endl;
+		}
+		
+		void deposite()
+		{
+			cout<<"Please Deposite Your Amount : ";
+			cin>>val;
+			balance = balance + val;
+			cout<<"Your Total Balance is : "<<balance<<endl;
+			cout<<"Thanks For The Deposite.\n\n\n";
+		}
+		
+		void withdraw()
+		{
+		
+			cout<<"Would you like to withdraw an amount ? y or n : ";
+			cin>>ch;
+			cout<<endl<<endl;
+			if(ch=='y' || ch=='Y')
+			{
+			
+				cout<<"How Much do you want to Withdraw ? ";
+				cin>>val;
+				balance = balance - val;
+			
+				cout<<"Your Available balance is : "<<balance<<endl<<endl;
+			}
+			else
+			{
+				exit;
+			}
+		}
+		
+		void Display()
+		{
+			cout<<"Hello "<<acc_holder_name<<endl;
+			cout<<"Your Final Amout is : "<<balance<<endl<<endl<<endl;
+			cout<<"----------Thank You----------"<<endl;
+		}
+		
+};
+
+int main()
+{
+	
+	bank b;
+	b.assign();
+	b.deposite();
+	b.withdraw();
+	b.Display();
+	
+	
+	return 0;
+}
---
+++ function_overloading.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Addition
+{
+	public : 
+	int total,x,y;
+	
+	Addition(int a, int b)
+	{
+		total = a + b;	
+		cout<<total<<endl;	
+	}
+	
+};
+
+int main()
+{
+	
+	return 0;
+}
